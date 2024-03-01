b74a80d2d19e3674abdad04dce1fc423ec19cc3d
mlionhart <lionrhart@gmail.com>
2024-03-01 10:18:28 -0700

    improved letter visibility in first p

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function openCity(typeName, elmnt, color) {
 }
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+const p = Array.from(document.querySelectorAll('p:first-of-type'));
+p.forEach(i => {
+  const text = i.innerText;
+  const updatedHtml = text.split('').map(char => {
+    if (char === char.toUpperCase() && char.match(/[A-Z]/i)) {
+      return `<span class="capital-letter">${char}</span>`;
+    } else {
+      return char;
+    }
+  }).join('');
+
+  i.innerHTML = updatedHtml;
+})
+
